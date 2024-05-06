2accd5c6852c86b961153664905c5d7da3df00a7
AngieJC <htk90uggk@outlook.com>
2024-05-06 14:30:31 +0800

    添加算法的描述文档，第一个采样算法在理论上应该不能再优化了。

--- gen_gprof_fig.sh
+++ gen_gprof_fig.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+###
+ # @Author: AngieJC htk90uggk@outlook.com
+ # @Date: 2024-05-06 13:52:57
+ # @LastEditors: AngieJC htk90uggk@outlook.com
+ # @LastEditTime: 2024-05-06 14:27:32
+ # @FilePath: /Crypto-math_Challenge_2024/gen_gprof_fig.sh
+### 
 
 TIME=$(date +"%Y_%m_%d_%H_%M_%S")
-FILENAME=./gprof_figs/test_time_${TIME}.pdf
+FILENAME=./gprof_figs/test_time_${TIME}.eps
 
-gprof test_time gmon.out | gprof2dot -n0 -e0 | dot -Tpdf -o "$FILENAME"
+gprof test_time gmon.out | gprof2dot | dot -Teps -o "$FILENAME"
